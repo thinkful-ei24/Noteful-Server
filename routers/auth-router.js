75de5f4ec0e4e7517d5a4b789f90949bcaa1d938
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-24 14:24:24 -0500

    Create auth router

---
+++ routers/auth-router.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+'use strict';
+
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const { JWT_EXPIRY, JWT_SECRET } = require('../config');
+
+const router = express.Router();
+const options = { session: false, failWithError: true };
+const localAuth = passport.authenticate('local', options);
+const jwtAuth = passport.authenticate('jwt', options);
+
+const createAuthToken = function(user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY,
+    algorithm: 'HS256'
+  });
+};
+
+module.exports = createAuthToken;
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
+});
+
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
+});
+
+module.exports = router;
